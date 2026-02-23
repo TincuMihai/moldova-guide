fd427a36bf10ef7accb0e858f0becf000e6cfc61
Popov Deenis <aaadpa0001@gmail.com>
2026-02-23 11:02:59 +0200

    add barrel export for all 6 service modules

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,6 @@
+export { authService } from './authService';
+export { attractionService } from './attractionService';
+export { tourService } from './tourService';
+export { bookingService } from './bookingService';
+export { tripService } from './tripService';
+export { savedPlacesService } from './savedPlacesService';
