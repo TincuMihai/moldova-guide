b2da65024b19aa352649cc6765067661423ada9c
Targus Vasile <vasile.targus@isa.utm.md>
2026-02-24 13:32:27 +0200

    wrap App with MaintenanceProvider in entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { AuthProvider } from './context/AuthContext';
+import { MaintenanceProvider } from './context/MaintenanceContext';
 import App from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <MaintenanceProvider>
+        <App />
+      </MaintenanceProvider>
+    </AuthProvider>
   </StrictMode>,
 );
