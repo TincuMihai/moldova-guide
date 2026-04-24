c5db07b2b09decedced0bb2cb5189c291aa13a3e
Popov Denis <aaadpa0001@gmail.com>
2026-04-24 09:49:20 +0300

    feat(data): add UserContext — DbSet<UserData> Users

---
+++ MoldovaGuide.DataAccess/Context/UserContext.cs
@@ -0,0 +1,15 @@
+using MoldovaGuide.Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoldovaGuide.DataAccess.Context
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<UserData> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbSession.ConnectionString);
+        }
+    }
+}
