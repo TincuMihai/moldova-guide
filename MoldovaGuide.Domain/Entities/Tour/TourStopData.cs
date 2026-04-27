b8c9973c86d86d08e9cc4f536d2d645676cefdba
Tincu MIhai <tincumichael@gmail.com>
2026-04-27 11:20:33 +0300

    feat(domain): add [ForeignKey] and Tour navigation property to TourImg and TourStopData

--- MoldovaGuide.Domain/Entities/Tour/TourStopData.cs
+++ MoldovaGuide.Domain/Entities/Tour/TourStopData.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+ 
 namespace MoldovaGuide.Domain.Entities.Tour
 {
     public class TourStopData
@@ -8,19 +8,23 @@ namespace MoldovaGuide.Domain.Entities.Tour
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+ 
+        // FK catre Tour
         public int TourId { get; set; }
-
+ 
+        [ForeignKey("TourId")]
+        public TourData Tour { get; set; }
+ 
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
-
+ 
         [StringLength(500)]
         public string? Description { get; set; }
-
+ 
         [StringLength(20)]
         public string Duration { get; set; }
-
+ 
         public int SortOrder { get; set; }
     }
 }
