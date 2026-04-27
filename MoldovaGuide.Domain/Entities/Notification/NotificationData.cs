888c7260bb28b93399abd1c2eed59cf807e3cb5b
Popov Denis <aaadpa0001@gmail.com>
2026-04-27 11:06:57 +0300

    feat(domain): add [ForeignKey] and User navigation property to NotificationData

--- MoldovaGuide.Domain/Entities/Notification/NotificationData.cs
+++ MoldovaGuide.Domain/Entities/Notification/NotificationData.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+using MoldovaGuide.Domain.Entities.User;
+ 
 namespace MoldovaGuide.Domain.Entities.Notification
 {
     public class NotificationData
@@ -8,18 +9,22 @@ namespace MoldovaGuide.Domain.Entities.Notification
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+ 
+        // FK catre User
         public int UserId { get; set; }
-
+ 
+        [ForeignKey("UserId")]
+        public UserData User { get; set; }
+ 
         [Required]
         [StringLength(500)]
         public string Text { get; set; }
-
+ 
         [StringLength(20)]
         public string Type { get; set; }
-
+ 
         public bool IsRead { get; set; }
-
+ 
         [DataType(DataType.Date)]
         public DateTime CreatedAt { get; set; }
     }
